3d440c2893340e4c4d18a057dce043fe00f84b5a
mynameispond <peerapolwongping@gmail.com>
2024-10-31 14:22:18 +0700

    is_image added

--- function.php
+++ function.php
@@ -304,3 +304,14 @@ function fn_un_serialize($str)
 
 	return unserialize($str);
 }
+
+function is_image($path)
+{
+	$a = getimagesize($path);
+	$image_type = $a[2];
+
+	if (in_array($image_type, array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_BMP))) {
+		return true;
+	}
+	return false;
+}
